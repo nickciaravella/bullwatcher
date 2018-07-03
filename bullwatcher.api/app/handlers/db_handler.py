b149136933628d96f0e82a5adc38c9a855948f0e
Nick Ciaravella <nciaravella@lyft.com>
2018-07-03 14:22:42 -0700

    Change stock metadata api to take ticker

--- bullwatcher.api/app/handlers/db_handler.py
+++ bullwatcher.api/app/handlers/db_handler.py
@@ -1,8 +1,13 @@
-from app.database import models
+from application import db
+from app.database.models import StockMetadata
+from sqlalchemy import func
 
 # Handler to read random data from the database. It will change over time.
-def stock_metadata():
-    return [{ 'ticker': m.ticker,
-               'company_name': m.company_name,
-               'market_cap': m.market_cap,
-               'sector': m.sector } for m in models.StockMetadata.query.all() ]
+def stock_metadata(ticker):
+    m = StockMetadata.query.filter(func.lower(StockMetadata.ticker) == func.lower(ticker)).one()
+    return {
+        'ticker': m.ticker,
+        'company_name': m.company_name,
+        'market_cap': m.market_cap,
+        'sector': m.sector
+    }
--- bullwatcher.api/app/routes.py
+++ bullwatcher.api/app/routes.py
@@ -8,11 +8,12 @@ def setup_routes(app):
     def stock_history(ticker):
         return jsonify(stocks_handler.get_stock_history(ticker))
 
+
     @app.route('/sync-stocks/<int:count>')
     def sync_stocks(count):
         return jsonify([s.to_json() for s in stocks_sync.sync(count)])
 
 
-    @app.route('/db/stock-metadata')
-    def db_stock_metadata():
-        return jsonify(db_handler.stock_metadata())
+    @app.route('/db/stock-metadata/<ticker>')
+    def db_stock_metadata(ticker):
+        return jsonify(db_handler.stock_metadata(ticker))
