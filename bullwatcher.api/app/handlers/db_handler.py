105a521bb314978898c81133c5f31cf0f5bcc5c4
Nick Ciaravella <nciaravella@lyft.com>
2018-07-03 22:36:00 -0700

    For some reason this fixes aws

--- bullwatcher.api/app/handlers/db_handler.py
+++ bullwatcher.api/app/handlers/db_handler.py
@@ -1,10 +1,10 @@
 from application import db
-from app.database.models import StockMetadata
+from app.database import models
 from sqlalchemy import func
 
 # Handler to read random data from the database. It will change over time.
 def stock_metadata(ticker):
-    m = StockMetadata.query.filter(func.lower(StockMetadata.ticker) == func.lower(ticker)).one()
+    m = models.StockMetadata.query.filter(func.lower(models.StockMetadata.ticker) == func.lower(ticker)).one()
     return {
         'ticker': m.ticker,
         'company_name': m.company_name,
