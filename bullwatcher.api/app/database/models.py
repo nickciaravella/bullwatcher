56f711b950557780c450e6e28bfd4947b5b0c920
Nick Ciaravella <nciaravella@lyft.com>
2018-08-12 22:30:20 -0700

    Added Users and PatternVotesDaily tables.

--- bullwatcher.api/app/database/models.py
+++ bullwatcher.api/app/database/models.py
@@ -1,6 +1,18 @@
 from application import db
 
 
+class Users(db.Model):
+    """
+    Table to hold users. The user_id column is the id provided by the identity provider
+    prepended with the identity provider itself. For example:
+    'google_<id>'
+    """
+    user_id         = db.Column(db.String(128), primary_key=True)
+    email           = db.Column(db.String(512))
+    full_name       = db.Column(db.String(512))
+    created_date    = db.Column(db.DateTime)
+
+
 class StockSyncStatus(db.Model):
     ticker          = db.Column(db.String(10), primary_key=True)
     synced_until    = db.Column(db.DateTime)
@@ -32,3 +44,14 @@ class PatternDaily(db.Model):
     date            = db.Column(db.Integer, primary_key=True)
     ticker          = db.Column(db.String(10), primary_key=True)
     flag_votes      = db.Column(db.Integer)
+
+
+class PatternVotesDaily(db.Model):
+    """
+    Table which tracks which users voted for which patterns. The total counts are cached in the PatternDaily table.
+    This is mainly useful for not allowing the same user to vote multiple times on the same pattern.
+    """
+    date            = db.Column(db.Integer, primary_key=True)
+    ticker          = db.Column(db.String(10), primary_key=True)
+    user_id         = db.Column(db.String(128), primary_key=True)
+    flag_vote       = db.Column(db.Integer)
