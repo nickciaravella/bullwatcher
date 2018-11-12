5ae5243ae10bdc1d680588cb5a3d92883624d42b
Nick Ciaravella <nciaravella@lyft.com>
2018-11-11 16:53:36 -0800

    Hiding navbar items behind 'preview' query param

--- bullwatcher.site/src/components/siteTemplate/NavBar.tsx
+++ bullwatcher.site/src/components/siteTemplate/NavBar.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
+import { withRouter } from "react-router";
+import { Link, RouteComponentProps } from "react-router-dom";
 import SearchBox from "src/SearchBox";
 
 
@@ -7,24 +8,39 @@ export interface INavBarProps {
     onTickerSearch: (ticker: string) => void;
 }
 
-export default class NavBar extends React.Component<INavBarProps, any> {
+class NavBar extends React.Component<RouteComponentProps<INavBarProps> & INavBarProps, any> {
 
     public render() {
-        const { onTickerSearch } = this.props;
+        const { location, onTickerSearch } = this.props;
+        const params = new URLSearchParams(location.search);
+        const isPreview: boolean = params.get('preview') &&  params.get('preview') === "true" ? true : false;
+        console.log(isPreview);
         return (
             <div className="pl-4 border bg-light" >
                 <div className="d-flex flex-row align-items-center" style={{ maxWidth: "1200px", margin: "auto"}}>
                     <SearchBox onSearchFunc={onTickerSearch} />
                     <nav className="navbar d-flex flex-row justify-content-between navbar-expand-sm bg-light">
+                    {
+                        isPreview &&
                         <ul className="nav  ml-3">
                             <li className="nav-item"><Link className="nav-link" to='/'>Overview</Link></li>
                             <li className="nav-item"><Link className="nav-link" to='/patterns/flags'>Flags</Link></li>
                             <li className="nav-item"><Link className="nav-link" to='/rankings'>Rankings</Link></li>
                             <li className="nav-item"><Link className="nav-link" to='/watchlists'>Watchlists</Link></li>
                         </ul>
-                    </nav>
+                    }
+                    {
+                        !isPreview &&
+                        <ul className="nav  ml-3">
+                            <li className="nav-item"><Link className="nav-link" to='/'>Overview</Link></li>
+                        </ul>
+                    }
+                   </nav>
                 </div>
             </div>
         );
     }
 }
+
+const NavBarWithRouter = withRouter(NavBar);
+export default NavBarWithRouter;
